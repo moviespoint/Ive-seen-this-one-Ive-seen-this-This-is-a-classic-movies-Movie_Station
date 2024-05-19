a15bc4a500ca6d049a4239b8d16e5b74b042ba8f
moviespoint <group.study.note@gmail.com>
2024-05-20 01:26:30 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.getElementById("notesButton").addEventListener("click", function() {
-  window.location.href = "https://panerusagar.github.io/studyhub";
+  window.location.href = "https://moviestation.github.io/web";
 })
